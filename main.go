09342b3482ce18d35bdabf7124abbf644e359f93
pxy05 <psky0695@gmail.com>
2025-10-08 04:28:36 +0100

    feat: added cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -20,6 +21,15 @@ func main() {
 	godotenv.Load()
 	supabase.InitDB(os.Getenv("SUPABASE_URL"), os.Getenv("SUPABASE_SERVICE_ROLE_KEY"))
 	router := gin.Default()
+	
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	router.GET("/users/applications/:user_id", endpoints.SBGetApplicationsByUserID)
 	router.POST("/applications", endpoints.SBAddApplication)
 	// router.GET("/applications/:id", endpoints.GetApplicationById)
